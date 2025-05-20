acd510399a20e72bcbc118d313153d0f1e44b48e
naruto <narutopandit220@gmail.com>
2025-05-20 16:27:00 +0530

    Update SignButton component styles for improved appearance and usability

--- frontend/src/components/ButtonSign.tsx
+++ frontend/src/components/ButtonSign.tsx
@@ -7,7 +7,7 @@ interface buttonType {
 
 export function SignButton({ title ,onClick}: buttonType) {
   return (
-    <button onClick={onClick} className="bg-gray-900 w-full max-w-sm min-w-[200px] mb-2">
+    <button onClick={onClick} className="text-gray-900 bg-white hover:bg-gray-100 border border-gray-200 focus:ring-4 focus:outline-none focus:ring-gray-100 font-medium rounded-lg text-sm px-5 py-2.5 text-center inline-flex items-center dark:focus:ring-gray-600 dark:bg-gray-800 dark:border-gray-700 dark:text-white dark:hover:bg-gray-700 me-2 mb-2">
       {title}
     </button>
   );
