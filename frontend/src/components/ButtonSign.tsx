f920f251ff4e1092a20dec9c29e9a4f359da1e1d
naruto <narutopandit220@gmail.com>
2025-05-20 16:38:51 +0530

    Adjust SignButton width for improved layout consistency

--- frontend/src/components/ButtonSign.tsx
+++ frontend/src/components/ButtonSign.tsx
@@ -7,7 +7,7 @@ interface buttonType {
 
 export function SignButton({ title ,onClick}: buttonType) {
   return (
-    <button onClick={onClick} className="text-white bg-[#050708] hover:bg-[#050708]/80 focus:ring-4 focus:outline-none focus:ring-[#050708]/50 font-medium rounded-lg text-sm px-5 py-2.5 text-center inline-flex items-center dark:hover:bg-[#050708]/40 w-10/12 dark:focus:ring-gray-600 me-2 mb-2">
+    <button onClick={onClick} className="text-white bg-[#050708] hover:bg-[#050708]/80 focus:ring-4 focus:outline-none focus:ring-[#050708]/50 font-medium rounded-lg text-sm px-5 py-2.5 text-center inline-flex items-center dark:hover:bg-[#050708]/40 w-8/12 dark:focus:ring-gray-600 me-2 mb-2">
       {title}
     </button>
   );
